c24d25a6d3e7e5b550a0fbc3308832ea812942bc
Kenneth Wilcox <wilcoxk@gmail.com>
2014-12-02 16:05:36 -0700

    Updated to vs2012 .net 4.5

--- Plugin 6/build.bat
+++ Plugin 6/build.bat
@@ -1,7 +1,8 @@
 @echo off
 REM This mess was hacked out by Ken
 REM Compile
-%WINDIR%\Microsoft.NET\Framework\v2.0.50727\jsc.exe /nologo /t:library /r:..\Interfaces\obj\Release\Interfaces.dll Plugin6.js
+REM %WINDIR%\Microsoft.NET\Framework\v2.0.50727\jsc.exe /nologo /t:library /r:..\Interfaces\obj\Release\Interfaces.dll Plugin6.js
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\jsc.exe /nologo /t:library /r:..\Interfaces\obj\Release\Interfaces.dll Plugin6.js
 REM move to our bin directory
 IF NOT EXIST Plugin6.dll GOTO NOFILE
 move Plugin6.dll ../bin
